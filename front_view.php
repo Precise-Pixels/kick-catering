d7525dc2d77c324a085f898880ca7cbd0710f3b3
jamesl1001 <jamesl1001@msn.com>
2013-11-03 14:22:50 +0000

    Add login button to every page

--- front_view.php
+++ front_view.php
@@ -6,7 +6,7 @@
     <?php require_once('includes/head.php'); ?>
 </head>
 <body>
-    <?php require_once('includes/logout_button.php'); ?>
+    <?php require_once('includes/loginout_button.php'); ?>
     <?php require_once("views/$file.php"); ?>
     <?php require_once('includes/scripts.php'); ?>
 </body>
--- includes/loginout_button.php
+++ includes/loginout_button.php
@@ -1,4 +1,6 @@
 <?php
 if(isset($_SESSION['status']) && $_SESSION['status'] == 'loggedin') {
     echo '<a href="logout"><button>Logout</button></a>';
+} else {
+    echo '<a href="login"><button>Login</button></a>';
 }
