981dd92e36570302743bad636304f4db758b52b4
faooful <faooful@gmail.com>
2014-01-22 15:44:12 +0000

    Starts of the mobile view of the homepge

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,12 @@
-<header>HEADER</header>
+<header>
+    <img class="kicklogo" src="img/logo.png"/>
+    <nav>
+        <ul>
+            <li><a href="#">home</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">services</a></li>
+            <li><a href="#">recruitment</a></li>
+            <li><a href="#">contact</a></li>
+        </ul>
+    </nav>
+</header>
--- views/index.php
+++ views/index.php
@@ -1,1 +1,26 @@
-<h1>INDEX</h1>
+<!--<h1>INDEX</h1>-->
+<div class="imagegallery"> </div>
+
+<h1 class="mainheading">Main heading here</h1>
+<p class="indexdescription">PLACEHOLDERTEXT </p>
+
+<div class="typesofwork">
+    <div class="chefagency">
+        <img src="../img/chefagencyimg.png"/>
+        <h2>Chef Agency</h2>
+    </div>
+    <div class="consultancy">
+        <img src="../img/consultancyimg.png"/>
+        <h2>Consultancy</h2>
+    </div>
+    <div class="cateringcontracts">
+        <img src="../img/cateringcontractsimg.png"/>
+        <h2>Catering Contracts</h2>
+    </div>
+    <div class="procurements">
+        <img src="../img/procurementsimg.png"/>
+        <h2>Procurements</h2>
+    </div>
+</div>
+
+<p class="quote">PLACEHOLDERTEXT</p>
