4ded7d971aeafa7f9636aeb6c4b1c934d615edd6
jamesl1001 <jamesl1001@msn.com>
2014-02-04 23:57:23 +0000

    Remove unnecessary div from login button

--- includes/loginout_button.php
+++ includes/loginout_button.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_SESSION['status']) && $_SESSION['status'] == 'loggedin') {
-    echo '<a href="logout"><div id="login-btn" class="btn">LOGOUT</div></a>';
+    echo '<a href="logout" id="login-btn" class="btn">LOGOUT</a>';
 } else {
-    echo '<a href="login"><div id="login-btn" class="btn">LOGIN / REGISTER</div></a>';
+    echo '<a href="login" id="login-btn" class="btn">LOGIN / REGISTER</a>';
 }
