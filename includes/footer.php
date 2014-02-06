071bcaaa389f2022b09dfc42532fb0d09a6b237e
faooful <faooful@gmail.com>
2014-02-06 21:34:42 +0000

    layed out elements for the footer for mobile first.

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,17 @@
-<footer>FOOTER</footer>
+<footer>
+    <div class="footer-padding align-centre">
+        <div id="contact">
+            <h2>Fancy a loaf?</h2>
+            <h2>contact us on:</h2>
+            <h2>023423421421</h2>
+        </div>
+        <div id="contact-info">
+            <h2>we can talk about things</h2>
+            <h2>click here to send a message</h2>
+            <h2>click here to send a message</h2>
+            <h2>click here to send a message</h2>
+        </div>
+        <div id="map"></div>
+        <div id="copyright"><p>Kick Catering all rights reserved</p></div>
+    </div>
+</footer>
