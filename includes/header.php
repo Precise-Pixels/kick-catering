1735f08eaed16147f500ad284c29f8b6f9b89e62
Jacob Hammond <ss_clan_cod@hotmail.com>
2014-02-07 19:17:22 +0000

    Added titles to icons, added hidden text to anchors

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,12 @@
 <header>
     <div class="header-padding align-centre">
-        <a href="https://www.facebook.com/kickcatering" class="social icon facebook" target="_blank"></a>
-        <a href="https://twitter.com/KickCatering" class="social icon twitter" target="_blank"></a>
+        <a href="https://twitter.com/KickCatering" class="social icon twitter" target="_blank" title="Twitter">Twitter</a>
+        <a href="https://www.facebook.com/kickcatering" class="social icon facebook" target="_blank" title="Facebook">Facebook</a>
         <?php require_once('loginout_button.php'); ?>
         <a href="/" id="logo"><img src="img/logo.png"/></a>
         <p id="header-contact">Kick Catering | Address Line 1, Line Two, Third Line, ABC 1AB | 012345 123456</p>
         <input type="checkbox" id="menu-toggle"/>
-        <label for="menu-toggle" id="menu-btn" class="icon burger"></label>
+        <label for="menu-toggle" id="menu-btn" class="icon burger" title="Open / Close Menu"></label>
         <nav>
             <ul>
                 <li><a href="/">Home</a></li>
--- includes/loginout_button.php
+++ includes/loginout_button.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_SESSION['status']) && $_SESSION['status'] == 'loggedin') {
-    echo '<a href="logout" id="login-btn" class="icon loggedout">LOGOUT</a>';
+    echo '<a href="logout" id="login-btn" class="icon loggedout" title="Logout">LOGOUT</a>';
 } else {
-    echo '<a href="login" id="login-btn" class="icon loggedin">LOGIN / REGISTER</a>';
+    echo '<a href="login" id="login-btn" class="icon loggedin" title="Login / Register">LOGIN / REGISTER</a>';
 }
