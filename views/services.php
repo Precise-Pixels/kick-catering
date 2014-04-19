e28ad2212fd1f981c9d201d9cbc88944b689256c
jamesl1001 <jamesl1001@msn.com>
2014-04-19 23:29:01 +0100

    Add placeholder markup for Services page

---
+++ views/services.php
@@ -0,0 +1,23 @@
+<section class="l-grey">
+    <div class="section-padding align-centre">
+
+        <h1>Services</h1>
+
+        <a id="agency"></a>
+        <h2>Agency</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae magna id velit hendrerit tempus. Quisque laoreet urna ut sagittis vestibulum.</p>
+
+        <a id="consultancy"></a>
+        <h2>Consultancy</h2>
+        <p>Sed vitae erat at lorem interdum convallis. Phasellus pharetra quam augue, in consequat orci malesuada non. Cras sollicitudin non nisl sed pharetra. Integer imperdiet est tellus.</p>
+
+        <a id="contracts"></a>
+        <h2>Contracts</h2>
+        <p>Sed varius bibendum posuere. Nunc ut adipiscing neque. Sed eu elementum nisl. Nam aliquet diam sed mauris aliquam consequat. Quisque fermentum fringilla volutpat. Integer varius nisi eros, nec vestibulum mauris aliquam sed.</p>
+
+        <a id="procurements"></a>
+        <h2>Procurements</h2>
+        <p>Morbi placerat varius dolor, id pulvinar orci rutrum sodales. Fusce vulputate a nisl sit amet fringilla.</p>
+
+    </div>
+</section>
