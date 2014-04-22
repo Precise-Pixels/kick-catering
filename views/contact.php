149716ace74df53ca289fecbcda32f4b0745e291
jamesl1001 <jamesl1001@msn.com>
2014-04-22 18:03:09 +0100

    Add Contact page with initial copy

---
+++ views/contact.php
@@ -0,0 +1,11 @@
+<section class="l-grey">
+    <div class="section-padding align-centre">
+
+        <h1>Contact</h1>
+
+        <p>We would love to hear from you. Please contact us by email and we'll phone you back at your convenience.</p>
+
+        <p><a href="mailto:info@kickcatering.co.uk">info@kickcatering.co.uk</a></p>
+
+    </div>
+</section>
