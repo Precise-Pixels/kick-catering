42d5cb6ddc67586e20c2c5339db4965fac5b4921
jamesl1001 <jamesl1001@msn.com>
2014-07-23 14:21:31 +0100

    Add footer content

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,17 @@
-<footer>FOOTER</footer>
+<footer>
+    <div class="align-centre">
+        <p id="sitemap">
+            <a href="/">Home</a>
+            |
+            <a href="about">About</a>
+            |
+            <a href="services">Services</a>
+            |
+            <a href="recruitment">Recruitment</a>
+            |
+            <a href="contact">Contact</a>
+        </p>
+        <p>Web design and development by <a href="http://precisepixels.co.uk/" target="_blank">Precise Pixels</a></p>
+        <p>&copy; 2014 Kick Catering, All Rights Reserved</p>
+    </div>
+</footer>
